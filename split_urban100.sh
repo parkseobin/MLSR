282074bf304a81fe90114d5d8197d72c9ee4b185
seobin <kor.p.sb@gmail.com>
2020-09-17 19:55:10 +0900

    split urban100 dataset

---
+++ split_urban100.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd Urban100
+mkdir train test validation
+mv img_00*.png test
+mv img_01*.png test
+mv img_02*.png test
+mv img_03*.png test
+mv img_040.png test
+mv img_04*.png validation
+mv img_050.png validation
+mv *.png train
